b02d232572c8ec66853e1cde69144a6e314d6258
Raspiduino <raspiduino@users.noreply.github.com>
2022-10-21 16:04:10 +0700

    New uARMRE

---
+++ Packagedll.bat
@@ -0,0 +1,11 @@
+"D:\Data\MRE SDK V3.0.00\tools\DllPackage.exe" "D:\Data\dev\uARMRE\uARMRE.vcproj"
+if %errorlevel% == 0 (
+ echo postbuild OK.
+  copy uARMRE.vpp ..\..\..\MoDIS_VC9\WIN32FS\DRIVE_E\uARMRE.vpp /y
+exit 0
+)else (
+echo postbuild error
+  echo error code: %errorlevel%
+  exit 1
+)
+
---
+++ custom_prebuild.bat
@@ -0,0 +1,14 @@
+REM custom_prebuild.bat 
+REM this is for call the ResEditor.exe to save the project resource, to generate ResID.h
+REM  for avoid ResID.h is modified after build, this will effect debug
+
+"D:\Data\MRE SDK V3.0.00\tools\ResEditor\CmdShell.exe" SAVE "D:\Data\dev\uARMRE\uARMRE.vcproj"
+if %errorlevel% == 0 (
+ echo prebuild OK.
+ exit 0
+) else (
+ echo prebuild error
+ echo error code: %errorlevel%
+ exit 1
+)
+
